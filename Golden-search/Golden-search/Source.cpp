7ee2073f0e8ecde37861113ad6ca76264d031910
masaya154t <masaya.f.318@gmail.com>
2015-11-11 18:59:33 +0900

    project change

--- Golden-search/Golden-search/Source.cpp
+++ Golden-search/Golden-search/Source.cpp
@@ -1,51 +1,114 @@
-#include <iostream>
-#define  EPS  1e-3//収束判定用
-using namespace std;
+/*********************************************/
+/* 黄金分割法によるy=x^4+3x^3+2x^2+1の最小値 */
+/*      coded by Y.Suganuma                  */
+/*********************************************/
+#include <stdio.h>
 
-//黄金比の値
-static const double GRATIO = 1.6180339887498948482045868343656;
+double snx(double);
+double gold(double, double, double, double *, int *, int, double(*)(double));
 
-//簡単な下に凸な関数f(x) = (x-1)^2。x=1で最小値0。
-double f(double x){
-	return (x - 1.0)*(x - 1.0);
+int main()
+{
+	double a, b, eps, val, x;
+	int ind, max;
+
+	a = -2.0;
+	b = -1.0;
+	eps = 1.0e-10;
+	max = 100;
+
+	x = gold(a, b, eps, &val, &ind, max, snx);
+
+	printf("x %f val %f ind %d\n", x, val, ind);
+
+	return 0;
 }
 
-//黄金分割法
-// f(x)が区間[lb,ub]で凸ならば、その極値を返す
-// 反復時に値が使いまわせるので、fの計算が1回のみでよい
-// ub: 下限    ub: 上限    K: 反復回数
-double goldenSectionSearch(double lb, double ub, int K){
-	double x1, x2; //内分点(1:GRATIOとGRATIO:1に分割する点)
-	double f1, f2; //内分点での値
+/****************/
+/* 関数値の計算 */
+/****************/
+double snx(double x)
+{
+	double f;
 
-	//内分点を計算
-	x1 = (ub - lb) / (GRATIO + 1.0) + lb;
-	f1 = f(x1);
-	x2 = (ub - lb) / GRATIO + lb;
-	f2 = f(x2);
+	f = x * x * x * x + 3.0 * x * x * x + 2.0 * x * x + 1.0;
 
-	for (int i = 0; i<K; i++){
-		if (f1 < f2){
-			ub = x2;
-			x2 = x1; f2 = f1;
-			x1 = (ub - lb) / (GRATIO + 1.0) + lb;
-			f1 = f(x1);
+	return f;
+}
+
+/******************************************/
+/* 黄金分割法(関数の最小値)               */
+/*      a,b : 初期区間 a < b              */
+/*      eps : 許容誤差                    */
+/*      val : 間数値                      */
+/*      ind : 計算状況                    */
+/*              >= 0 : 正常終了(収束回数) */
+/*              = -1 : 収束せず           */
+/*      max : 最大試行回数                */
+/*      fun : 関数値を計算する関数の名前  */
+/*      return : 結果                     */
+/******************************************/
+#include <math.h>
+
+double gold(double a, double b, double eps, double *val, int *ind, int max, double(*fun)(double))
+{
+	double f1, f2, fa, fb, tau, x = 0.0, x1, x2;
+	int count;
+
+	tau = (sqrt(5.0) - 1.0) / 2.0;
+	count = 0;
+	*ind = -1;
+	x1 = b - tau * (b - a);
+	x2 = a + tau * (b - a);
+	f1 = fun(x1);
+	f2 = fun(x2);
+
+	while (count < max && *ind < 0) {
+		count += 1;
+		if (f2 > f1) {
+			if (fabs(b - a) < eps && fabs(b - a) < eps*fabs(b)) {
+				*ind = 0;
+				x = x1;
+				*val = f1;
+			}
+			else {
+				b = x2;
+				x2 = x1;
+				x1 = a + (1.0 - tau) * (b - a);
+				f2 = f1;
+				f1 = fun(x1);
+			}
 		}
-		else{
-			lb = x1;
-			x1 = x2; f1 = f2;
-			x2 = (ub - lb) / GRATIO + lb;
-			f2 = f(x2);
+		else {
+			if (fabs(b - a) < eps && fabs(b - a) < eps*fabs(b)) {
+				*ind = 0;
+				x = x2;
+				*val = f2;
+				f1 = f2;
+			}
+			else {
+				a = x1;
+				x1 = x2;
+				x2 = b - (1.0 - tau) * (b - a);
+				f1 = f2;
+				f2 = fun(x2);
+			}
 		}
 	}
 
-	return (lb + ub) / 2.0;
-}
+	if (*ind == 0) {
+		*ind = count;
+		fa = fun(a);
+		fb = fun(b);
+		if (fb < fa) {
+			a = b;
+			fa = fb;
+		}
+		if (fa < f1) {
+			x = a;
+			*val = fa;
+		}
+	}
 
-
-int main(){
-
-	cout << goldenSectionSearch(-10.0, 10.0, 30) << endl;
-
-	return 0;
+	return x;
 }
